6c87cdf6930494a90bfaa410f84b2445b7cb251d
Ashwini Gosavi <120784052+ashwinigosavi27@users.noreply.github.com>
2023-02-23 13:38:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here!
 
+<script type="text/javascript">
+var get = document.querySelector('#scroll');
+var add = 1;
+var infiniteScroll = function() {
+ for (var i = 0; i = get.scrollHeight) {
+infiniteScroll();
+  }
+});
+infiniteScroll();
+</script>
