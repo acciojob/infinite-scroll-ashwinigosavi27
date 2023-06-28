0f6fc962b3d021f6921d067fff71125969e8f8c4
Ashwini Gosavi <120784052+ashwinigosavi27@users.noreply.github.com>
2023-06-28 21:11:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 //your code here!
+let totalList=10;
+let OlList=document.getElementById('infi-list');
 
-<script type="text/javascript">
-var get = document.querySelector('#scroll');
-var add = 1;
-var infiniteScroll = function() {
- for (var i = 0; i = get.scrollHeight) {
-infiniteScroll();
-  }
+window.addEventListener('load',()=>{
+    for(let i=1;i<=totalList;i++){
+        let li=document.createElement('li');
+        li.innerText=`Item ${i}`;
+        OlList.append(li);
+        }
 });
-infiniteScroll();
-</script>
+
+
+OlList.addEventListener('scroll',()=>{
+    // console.log('hello');
+    // console.log(OlList.scrollTop);
+    let top=Math.floor(OlList.scrollTop);
+//   console.log(top);
+//    console.log(OlList.scrollHeight);
+let height=OlList.offsetHeight;
+let offset=OlList.scrollTop;
+
+if(offset+height>=OlList.scrollHeight){
+    console.log('at bottom');
+
+    for(let i=1;i<=2;i++){
+        let li=document.createElement('li');
+        li.innerText=`Item ${++totalList}`;
+        OlList.append(li);
+        }
+}
+})
